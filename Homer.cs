49ea7ee2e3b0c64d770551ac0e78a6a92c761465
camilo <colonca1999@gmail.com>
2024-06-10 23:31:03 -0500

    open close violation primera version

---
+++ Homer.cs
@@ -0,0 +1,9 @@
+class Homer {
+  public void eat() {
+    Console.WriteLine("Homer Simpson eat");
+  }
+  public void takeBeer() {
+    Console.WriteLine("Homer Simpson take beer");
+  }
+}
+
---
+++ SimpsonsDrawer.cs
@@ -0,0 +1,5 @@
+class SimpsonsDrawer {
+  public void Draw(Homer homer) {
+    Console.WriteLine("Homer Simpson draw");
+  }
+}
